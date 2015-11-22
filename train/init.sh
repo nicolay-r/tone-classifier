1c0ece18761c3ce4a706aba83dcfb7cba27feb4a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-11-22 11:43:08 +0300

    Добавлен скрипт инициализации базы данных для проверки работы классификатора

---
+++ train/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./x2pg_table.py data/bank_train.xml
+./x2pg_table.py data/bank_test.xml
+./x2pg_table.py data/bank_test_etalon.xml
+./x2pg_data.py data/bank_train.xml
+./x2pg_data.py data/bank_test.xml
+./x2pg_data.py data/bank_test_etalon.xml
+
