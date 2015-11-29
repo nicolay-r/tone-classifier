0f11fcc3d4512dd5dd8ccd7a2c57ad45f64b9b4c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-11-29 21:39:48 +0300

    Исправлены отступы в файле

--- train/x2pg_data.py
+++ train/x2pg_data.py
@@ -6,39 +6,35 @@ from psycopg2 import connect
 
 # Getting Row of a table from xmlFile
 def getRow(tableNode):
-        row = {}
-        columnNode = tableNode.children
-        while columnNode is not None:
-                if columnNode.type == "element":
-                        colName = columnNode.prop("name")
-                        colValue = columnNode.getContent().strip()
-                        # Check that text is not "NULL"
-                        if (colValue != "NULL"):
-                                # String data type
-                                row[colName] = "\'%s\'"%(colValue.replace('\'', '\'\''))
-                columnNode = columnNode.next
-        return row
+    row = {}
+    columnNode = tableNode.children
+    while columnNode is not None:
+        if columnNode.type == "element":
+            colName = columnNode.prop("name")
+            colValue = columnNode.getContent().strip()
+            # Check that text is not "NULL"
+            if (colValue != "NULL"):
+                # String data type
+                row[colName] = "\'%s\'"%(colValue.replace('\'', '\'\''))
+        columnNode = columnNode.next
+    return row
 
 # Insert Row into table
 def insertRow(cursor, tableName, row):
-        args = "";
-        argv = "";
-        for colName in row.keys():
-                if (args != ""):
-                        args += ","
-                        argv += ","
-                args += str(colName)
-                argv += str(row[colName])
+    args = ""
+    argv = ""
+    for colName in row.keys():
+        if (args != ""):
+            args += ","
+            argv += ","
+        args += str(colName)
+        argv += str(row[colName])
 
-        if (not cursor.closed):
-                cursor.execute( "INSERT INTO %s(%s) VALUES(%s);"%(
-                        tableName, args, argv))
-        else:
-                raise Exception('Cursor closed')
+    cursor.execute( "INSERT INTO %s(%s) VALUES(%s);"%(tableName, args, argv))
 
 if (len(sys.argv) == 1):
-        print "usage: export_data <xmlFile>"
-        exit(0)
+    print "usage: export_data <xmlFile>"
+    exit(0)
 # Parsing Xml File
 xmlFile = sys.argv[1]
 
@@ -51,7 +47,7 @@ databaseNode = ctx.xpathEval("/pma_xml_export/database")[0]
 dbName = databaseNode.prop("name").lower()
 
 connSettings = """dbname=%s user=%s password=%s host=%s"""%(
-        dbName, "postgres", "postgres", "localhost")
+    dbName, "postgres", "postgres", "localhost")
 
 conn = connect(connSettings)
 cursor = conn.cursor()
@@ -60,8 +56,8 @@ cursor = conn.cursor()
 tables = ctx.xpathEval("/pma_xml_export/database/table")
 
 for tableNode in tables:
-        row = getRow(tableNode)
-        insertRow(cursor, tableNode.prop("name"), row)
+    row = getRow(tableNode)
+    insertRow(cursor, tableNode.prop("name"), row)
 
 cursor.close()
 conn.commit()
