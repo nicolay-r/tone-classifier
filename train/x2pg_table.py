a523f4b4f4f31c54df846e1f08914d9f0ce9616d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-11-30 11:28:28 +0300

    Изменены отступы

--- train/x2pg_table.py
+++ train/x2pg_table.py
@@ -7,8 +7,8 @@ from psycopg2 import connect, extensions
 from db_converter import parse
 
 if len(sys.argv) == 1:
-        print 'usage: train_table <xmlFile>'
-        exit(0)
+    print 'usage: train_table <xmlFile>'
+    exit(0)
 # Getting SQL script from xmlFile
 xmlFile = sys.argv[1]
 
@@ -17,18 +17,18 @@ ctx = doc.xpathNewContext()
 ctx.xpathRegisterNs("pma", "http://www.phpmyadmin.net/some_doc_url/")
 
 createTable = ctx.xpathEval("""/pma_xml_export/pma:structure_schemas/
-        pma:database/pma:table""")[0].getContent()
+    pma:database/pma:table""")[0].getContent()
 
 createTable = createTable.replace('`', '\"')
 
 # Convert mysql table to postgreSQL
 with open(".mysql", "w") as f:
-        f.write(createTable)
+    f.write(createTable)
 
 parse(".mysql", ".pgsql")
 
 with open(".pgsql", "r") as f:
-        createTable = f.read()
+    createTable = f.read()
 
 createTable = createTable.replace("AUTO_INCREMENT", "")
 
@@ -36,20 +36,20 @@ createTable = createTable.replace("AUTO_INCREMENT", "")
 database = ctx.xpathEval("/pma_xml_export/database")[0].prop('name').lower()
 
 connSettings = "dbname=%s user=%s password=%s host=%s"%(
-        "postgres", "postgres", "postgres", "localhost")
+    "postgres", "postgres", "postgres", "localhost")
 conn = connect(connSettings)
 cursor = conn.cursor()
 
 try:
-        conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-        cursor.execute("CREATE DATABASE %s;"%(database))
-        cursor.close()
-        conn.close()
+    conn.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor.execute("CREATE DATABASE %s;"%(database))
+    cursor.close()
+    conn.close()
 except:
-        pass
+    pass
 
 connSettings = "dbname=%s user=%s password=%s host=%s"%(
-        database, "postgres", "postgres", "localhost")
+    database, "postgres", "postgres", "localhost")
 conn = connect(connSettings)
 cursor = conn.cursor()
 cursor.execute(createTable)
