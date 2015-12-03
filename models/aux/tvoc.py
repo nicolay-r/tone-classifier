989f4fdf974b2731c26db6ed6d9808d30f0f5923
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-12-03 20:43:41 +0300

    Рефакторинг tvoc.py

--- models/aux/tvoc.py
+++ models/aux/tvoc.py
@@ -4,7 +4,7 @@ import operator
 
 class TermVocabulary:
     def getTermIndex(self, term):
-        return self.term_ind[term]
+        return self.term_index[term]
 
     def getTermInDocsCount(self, term):
         return self.doc_count[term]
@@ -25,7 +25,7 @@ class TermVocabulary:
     def getIndexes(self, terms):
         indexes = []
         for term in terms:
-            indexes.append(self.term_ind[term])
+            indexes.append(self.term_index[term])
         return indexes
 
     def getNewIndex(self):
@@ -34,8 +34,8 @@ class TermVocabulary:
         return result
 
     def add_feature(self, feature):
-        if not(feature in self.term_ind):
-            self.term_ind[feature] = self.getNewIndex()
+        if not(feature in self.term_index):
+            self.term_index[feature] = self.getNewIndex()
 
     def add_doc(self, terms):
         used = []
@@ -46,9 +46,9 @@ class TermVocabulary:
                 self.voc_count[term] = 1
             else:
                 self.voc_count[term] += 1
-            # update term_ind
-            if not(term in self.term_ind):
-                self.term_ind[term] = self.getNewIndex()
+            # update term_index
+            if not(term in self.term_index):
+                self.term_index[term] = self.getNewIndex()
             # update doc_count
             if not(term in used):
                 if term in self.doc_count:
@@ -58,7 +58,7 @@ class TermVocabulary:
                 used.append(term)
 
     def __init__(self):
-        self.term_ind = {}
+        self.term_index = {}
         self.voc_count = {}
         self.doc_count = {}
         self.documents = 0
