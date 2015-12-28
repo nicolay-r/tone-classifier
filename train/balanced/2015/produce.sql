9c86bcd87961104c160a1e7b62957c2f80bf7c78
Nicolay Rusnachenko <kolyarus@yandex.ru>
2015-12-28 22:48:58 +0300

    Рефакторинг фильтрующих скриптов

--- train/balanced/2015/produce.sql
+++ train/balanced/2015/produce.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS bank_train_balanced;
 CREATE TABLE bank_train_balanced AS
     SELECT * FROM bank_train_noname;
-ALTER TABLE positive ADD COLUMN rank INT DEFAULT 1;
-ALTER TABLE negative ADD COLUMN rank INT DEFAULT -1;
+ALTER TABLE bank_positive ADD COLUMN rank INT DEFAULT 1;
+ALTER TABLE bank_negative ADD COLUMN rank INT DEFAULT -1;
 INSERT INTO bank_train_balanced(twitid, text, sberbank)
-    SELECT id, text, rank FROM positive limit(3224);
+    SELECT id, text, rank FROM bank_positive limit(3224);
 INSERT INTO bank_train_balanced(twitid, text, sberbank)
-    SELECT id, text, rank FROM negative limit(2519);
+    SELECT id, text, rank FROM bank_negative limit(2519);
 
 DROP TABLE IF EXISTS ttk_train_balanced;
 CREATE TABLE ttk_train_balanced AS
     SELECT * FROM ttk_train_noname;
 ALTER TABLE ttk_positive ADD COLUMN rank INT DEFAULT 1;
-ALTER TABLE ttk_negative ADD COLUMN rank INT DEFAULT -1;
+ALTER TABLE ttk_negative ADD COLUMN rank INT DEFAUL -1;
 INSERT INTO ttk_train_balanced(twitid, text, mts)
-    SELECT id, text, rank FROM ttk_positive limit(2202);
+    SELECT id, text, rank FROM ttk_positive limit(1336);
 INSERT INTO ttk_train_balanced(twitid, text, mts)
-    SELECT id, text, rank FROM ttk_negative limit(1685);
+    SELECT id, text, rank FROM ttk_negative limit(660);
--- train/balanced/load.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget "https://www.dropbox.com/s/vqul3qh3d0o1qyo/positive.csv?dl=1"
-wget "https://www.dropbox.com/s/944dfta73i6s95z/negative.csv?dl=1"
