5cdb878fa028f9dbdbc172d0fb4594801eb3d5ab
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-02-22 08:03:45 -0500

    Исправлен скрипт инициализации таблиц.

--- data/2015/init.sh
+++ data/2015/init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-./x2pg_table.py bank_train.xml
-./x2pg_table.py bank_test.xml
-./x2pg_table.py bank_test_etalon.xml
-./x2pg_.py bank_train.xml
-./x2pg_.py bank_test.xml
-./x2pg_.py bank_test_etalon.xml
+../x2pg_table.py bank_train.xml
+../x2pg_table.py bank_test.xml
+../x2pg_table.py bank_test_etalon.xml
+../x2pg_data.py bank_train.xml
+../x2pg_data.py bank_test.xml
+../x2pg_data.py bank_test_etalon.xml
 
-./x2pg_table.py ttk_train.xml
-./x2pg_table.py ttk_test.xml
-./x2pg_table.py ttk_test_etalon.xml
-./x2pg_.py ttk_train.xml
-./x2pg_.py ttk_test.xml
-./x2pg_.py ttk_test_etalon.xml
+../x2pg_table.py ttk_train.xml
+../x2pg_table.py ttk_test.xml
+../x2pg_table.py ttk_test_etalon.xml
+../x2pg_data.py ttk_train.xml
+../x2pg_data.py ttk_test.xml
+../x2pg_data.py ttk_test_etalon.xml
