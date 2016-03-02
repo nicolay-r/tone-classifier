2ad97a4c46fd7f6f472263665951c60d69af6314
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-03-02 15:57:57 -0500

    Добавлена генерация ttk лексикона, и скрипта для полной сборки bank и ttk лексиконов.

--- tools/lexicons/task_specific/init.sh
+++ tools/lexicons/task_specific/init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 psql -U postgres -h localhost -W -d romipdata -f bank.sql
-
 pushd .
     cd ../../pmieval
     ./pmieval.py bank_positive bank_negative bank_lexicon
+popd
+
+psql -U postgres -h localhost -W -d romipdata -f ttk.sql
+pushd .
+    cd ../../pmieval
+    ./pmieval.py ttk_positive ttk_negative ttk_lexicon
 popd
