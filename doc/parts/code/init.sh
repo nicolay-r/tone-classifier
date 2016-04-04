49645530466255815ffa6a62898e0fa70f5ac245
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-04-04 23:08:23 +0300

    Добавлена поддержка русского языка в комментариях кода. Задан префикс для листингов. Исправления в завписке.

--- doc/parts/code/init.sh
+++ doc/parts/code/init.sh
@@ -1,13 +1,13 @@
-# Install all dependecies.
+# Установка зависимостей.
 sudo apt-get install python-libxml2 python-psycopg2 python-pip postgresql-9.3 \
     g++ nodejs npm nodejs-legacy unzip
 sudo pip install pymystem3
 
-# Downloading and compile SVM library.
+# Загрузка и компиляция библиотеки LibSVM.
 git clone https://github.com/cjlin1/libsvm
 make -C libsvm
 make -C libsvm/python
 
-# Install eval package -- script which estimates a model result quality.
+# Установка пакета eval -- скрипт оценки качества работы модели.
 cd eval
 npm install
