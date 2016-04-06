f1a079d2bfc87d70f000cc7ec73e0013da5f687d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-04-07 00:51:56 +0300

    Исправлена реализация, которая теперь кэширует содержимое лексиконов. Производительность существенно повысилась

--- models/aux/lexicon.py
+++ models/aux/lexicon.py
@@ -27,6 +27,18 @@ class Lexicon:
             result = result[0]
         return result
 
+    def get_all_tones_from_table(self):
+        print "Caching [%s]..."%(self.name)
+        cursor = self.connection.cursor()
+        cursor.execute("""SELECT {name}, {value} FROM {table}""".format(
+            name = self.term_column_name, value = self.value_column_name,
+            table = self.table))
+
+        value = cursor.fetchone()
+        while value is not None:
+            self.cache[value[0].decode('utf-8')] = float(value[1])
+            value = cursor.fetchone()
+
     def get_tone(self, term):
         unicode_term = Lexicon.to_unicode(term)
         tone = 0
@@ -38,13 +50,12 @@ class Lexicon:
         elif (unicode_term[0] == '+'):
             unicode_term = unicode_term[1:]
 
+        if (self.cached == False):
+            self.get_all_tones_from_table()
+            self.cached = True
+
         if (unicode_term in self.cache):
             tone = self.cache[unicode_term]
-        else:
-            result = self.get_tone_from_table(unicode_term)
-            if not (result is None):
-                tone = float(result)
-                self.cache[unicode_term] = tone
 
         # invert term tonality in case of negative mark '-'
         if (invert_tone):
@@ -55,6 +66,7 @@ class Lexicon:
     def __init__(self, configpath, table, name,
         term_column_name, value_column_name):
 
+        self.cached = False
         self.cache = {}
 
         # init config file
