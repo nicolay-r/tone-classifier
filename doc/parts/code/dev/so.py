44a2179260fa30a971673fc8d51918a76f5d08cd
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-06-01 12:02:00 +0400

    Добавлено описание сохранения лексикона

--- doc/parts/code/dev/so.py
+++ doc/parts/code/dev/so.py
@@ -3,6 +3,6 @@ def SO(tv1, dv1, tv2, dv2):
     r1 = {}
     all_terms = merge_two_lists(tv1.get_terms(), tv2.get_terms())
     for term in all_terms:
-        r1[to_unicode(term)] = PMI(term, dv1, dv2) - PMI(term, dv2, dv1)
+        r1[term] = PMI(term, dv1, dv2) - PMI(term, dv2, dv1)
     r1 = sorted(r1.items(), key=operator.itemgetter(1), reverse=True)
     return r1
