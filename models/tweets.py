41c6f92f5b3596445fc1a8a60341e190a8675c10
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-12-24 19:25:02 +0300

    Исправлена сборка для ttc задачи, удалены комментарии

--- models/tweets.py
+++ models/tweets.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 TASKTYPE_BANK = 'bank'
-TASKTYPE_TCC = 'tkk'
+TASKTYPE_TCC = 'ttk'
 
 
 def get_score_columns(task_type):
@@ -11,28 +11,6 @@ def get_score_columns(task_type):
     if (task_type == TASKTYPE_TCC):
         return ['beeline', 'mts', 'megafon', 'tele2', 'rostelecom', 'komstar',
                 'skylink']
-
-
-# Заменить на check_row
-# def next_row(cursor, expected_score, vector_type):
-#     # filtering rows
-#     if vector_type == 'train':
-#         notFound = True
-#         while notFound:
-#             notFound = False
-#             next_row = cursor.fetchone()
-#             if next_row is not None:
-#                 # skip text and id
-#                 for i in range(2, len(next_row)):
-#                     if (next_row[i] is not None and is_int(next_row[i]) and
-#                        expected_score != int(next_row[i])):
-#                         notFound = True
-#                         break
-#             else:
-#                 break
-#     elif vector_type == 'test':
-#         next_row = cursor.fetchone()
-#     return next_row
 
 
 def is_int(s):
