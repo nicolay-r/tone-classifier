922ea480d3221fbbc3a7ef0a82184b972d4771d1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2016-12-26 15:13:45 +0400

    Удалены неиспользуемые каталоги и файлы. Исправлена bow модель

---
+++ models/bow_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# this
+import utils
+
+
+def vectorizer(labeled_message, term_voc, doc_voc):
+    """
+    Vector builder
+
+    Arguments:
+    ---------
+        labeled_message -- dictionary with the following fields:
+                           {score, id, terms, features}
+        term_voc -- vocabulary of terms
+        doc_voc -- vocabulary of documents
+
+    Returns
+    ------
+        vector -- {index1: value1, ... , indexN: valueN}
+    """
+    vector = {}
+
+    features = labeled_message['features']
+    for feature_name in features.keys():
+        index = term_voc.get_term_index(feature_name)
+        vector[index] = features[feature_name]
+
+    terms = labeled_message['terms']
+    for term in terms:
+        index = term_voc.get_term_index(term)
+        vector[index] = bag_of_words(term, terms)
+    return vector
+
+
+def bag_of_words(term, doc_terms):
+    """
+    calculate bag_of_words measure for a doc
+    """
+    return doc_terms.count(term) * 1.0
+
+
+utils.vectorization_core(vectorizer)
