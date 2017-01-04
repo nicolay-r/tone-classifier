f91916447547fa11cfbb44a78aca405e1addce74
Nicolay Rusnachenko <kolyarus@yandex.ru>
2017-01-04 22:15:54 +0300

    Удалены вспомогательные инструменты

--- tools/lexicons/rubtsova/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./full_extract.py ../../../data/rubtsova/negative.csv romipdata rubtsova_negative
-./full_extract.py ../../../data/rubtsova/positive.csv romipdata rubtsova_positive
-pushd .
-    cd ../../pmieval
-    ./pmieval.py rubtsova_positive rubtsova_negative rubtsova_lexicon
-popd
