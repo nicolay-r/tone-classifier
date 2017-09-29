dde1f8988f7b2ad9af834bb078954ef4fd2aba6a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2017-09-29 17:10:35 +0300

    Added batching

--- models/utils.py
+++ models/utils.py
@@ -33,7 +33,7 @@ def init_logger():
 
 # TODO: pass here the output filepath based on the parameter from argv.
 def train_network(model, X, y, output, reg_lambda=10e-4, min_reg_lambda=10e-7,
-                  callback=None, epoch_delta=5):
+                  callback=None, epoch_delta=5, batch_size=8, epochs_count=20):
     """
     Train neural network model, based on the 'train' set.
 
@@ -52,6 +52,10 @@ def train_network(model, X, y, output, reg_lambda=10e-4, min_reg_lambda=10e-7,
             callback which calls every certain amount of 'epoch_delta'
         epoch_delta: int
             amount of epochs will be passed before 'callback' has been called.
+        batch_size: int
+            batch size.
+        epochs_count: int
+            amount of epochs.
         output : str
             output filepath where to store the model.
     Returns:
@@ -61,10 +65,13 @@ def train_network(model, X, y, output, reg_lambda=10e-4, min_reg_lambda=10e-7,
     i_rl = reg_lambda
     p_loss = model.calculate_loss(X, y)
     rl_div = 0.5
-    logging.info("initial loss: %f" % (p_loss))
+
+    logging.info("batch size: %f" % (batch_size))
+    logging.info("epochs count: %f" % (epochs_count))
+    logging.info("batch size: %f" % (batch_size))
 
     epoch = 0
-    while epoch < 10000:
+    while epoch < epochs_count:
 
         logging.info('epoch: %s' % (epoch))
 
@@ -72,28 +79,37 @@ def train_network(model, X, y, output, reg_lambda=10e-4, min_reg_lambda=10e-7,
         X = X[p]
         y = y[p]
 
-        model.sgd_step(X, y, reg_lambda)
+        for i in range(len(X) / batch_size):
 
-        c_loss = model.calculate_loss(X, y)
-        while (c_loss >= p_loss and reg_lambda > min_reg_lambda):
+            start = i * batch_size
+
+            Xb = X[start:(start + batch_size)]
+            yb = y[start:(start + batch_size)]
+
+            model.sgd_step(Xb, yb, reg_lambda)
+
+            c_loss = model.calculate_loss(Xb, yb)
+            while (c_loss >= p_loss and reg_lambda > min_reg_lambda):
+                logging.info("current_loss: %f" % (c_loss))
+                logging.info("rollback sgd_step, lost=%f. reg_lambda=%f" %
+                            (c_loss, reg_lambda))
+
+                model.rollback_step(reg_lambda)
+                reg_lambda *= rl_div
+                model.apply_step(reg_lambda)
+                c_loss = model.calculate_loss(Xb, yb)
+
             logging.info("current_loss: %f" % (c_loss))
-            logging.info("rollback sgd_step, where lost=%f. reg_lambda=%f" %
-                         (c_loss, reg_lambda))
 
-            model.rollback_step(reg_lambda)
-            reg_lambda *= rl_div
-            model.apply_step(reg_lambda)
-            c_loss = model.calculate_loss(X, y)
+            if (reg_lambda < min_reg_lambda):
+                reg_lambda = i_rl
+                logging.info("reset reg_lambda: %f to %f" % (reg_lambda, i_rl))
 
-        logging.info("current_loss: %f" % (c_loss))
-
-        if (reg_lambda < min_reg_lambda):
-            reg_lambda = i_rl
-            logging.info("reset reg_lambda: %f to %f" % (reg_lambda, i_rl))
+            p_loss = c_loss
 
         logging.info('save model: %s' % (output))
         model.save(output)
-        p_loss = c_loss
+
         epoch += 1
 
         if (epoch % epoch_delta == 0 and callback is not None):
